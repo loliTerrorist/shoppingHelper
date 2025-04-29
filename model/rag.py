3ee82007df8af4e012ac78f6596401d03dbe333f
loliTerrorist <ninjago.yoyo@gmail.com>
2025-04-29 16:42:48 +0800

    added requirments, added model

--- model/rag.py
+++ model/rag.py
@@ -1,19 +1,19 @@
 import transformers
-import faiss
+from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
 import numpy as np
 import sys, os
 import logging
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 config = {
-    "model": "",
-    "tokenizer": "",
+    "model": "THUDM/GLM-4-9B-0414",
     "chunk_size": 500,
     "chunk_overlap": 100,
     "similarity_mode": "cosine",
     "threshold": 0.1,
     "top_k": 3
 }
+
 
 ref_directory  = "/home/loliterrorist/Desktop/shoppingHelper/reference_file"
 
@@ -32,4 +32,10 @@ def load_files(dir):
 # def retrieve
 if __name__ == "__main__":
     text = load_files(ref_directory)
-    print(text)
+    tokenizer = AutoTokenizer.from_pretrained(config["model"], trust_remote_code = True)
+    model = AutoModel.from_pretrained(config["model"], trust_remote_code = True)
+    user_input = input()
+    message = {"role": "user", "content": user_input}
+    history = []
+    response, history = model.chat(tokenizer, user_input, history = history)
+    print(response)
